ee00d675463ceb7374b5b4a3b8e53a3c58c0d8c7
Kevin Williams <44032970+kwill1992@users.noreply.github.com>
2020-10-24 16:47:30 -0400

    working distances for top 10 cities

--- Get Distances.R
+++ Get Distances.R
@@ -6,7 +6,7 @@ cities <- read_csv("my_top_50.csv")
 
 
 #### find cities online - maybe with lat/long? ####
-
+# To Do: get top xxx cities by population from online webscrape
 
 
 #### link to google API ####
@@ -25,38 +25,23 @@ library(ggmap)
 
 
 #### Get top 10 to work and save ####
-# get top 10 by population from cities
+# Get top 10 by population from cities
+# Get percent of Population by city from total Population.
+# If 4,000 cars moving per month, with a ratio by Population, calc cars by city.
+num_cars_month <- 4000
 library(dplyr)
 cities_top10 <- cities %>% 
   arrange(desc(Population)) %>% # sort Tibble by Population and descending
-  slice_head(n = 10) # Get only the first n rows.
+  slice_head(n = 10) %>% # Get only the first n rows.
+  mutate(freq = Population / sum(Population)) %>% # percent of Population
+  mutate(num_cars = round(freq * num_cars_month,0))
 
-# Create empty Tibble with 3 columns
-distances <- tibble(from=character(0),to=character(0),distance=numeric(0))
-# set from and to as list of cities
-from <- cities_top10$`City, State`
-to <- cities_top10$`City, State`
-# loop to get map distances
-x <- 1
-for (i in 1:length(from)){
-  for (j in 1:length(to)){
-    if (i == j){
-      next
-    }
-    distances[x,1] <- from[i]
-    distances[x,2] <- to[j]
-    distances[x,3] <- i*j
-    x <- x + 1
-  }
-  
-}
-
-
-distances <- tibble(from=character(0),to=character(0),distance=numeric(0))
-# set 'from' and 'to' as list of cities
-from <- cities_top10$`City, State`
-to <- cities_top10$`City, State`
-# loop to get map distances
+# Create empty Tibble with 9 columns
+distances <- tibble(from=character(0),to=character(0),distance=numeric(0),
+                      from_population=numeric(0),from_pop_ratio=numeric(0),
+                      from_num_cars=numeric(0),to_population=numeric(0),
+                      to_pop_ratio=numeric(0),to_num_cars=numeric(0))
+# For loop to calculate distances between each city combination
 x <- 1
 for (i in 1:length(from)){
   for (j in 1:length(to)){
@@ -64,21 +49,20 @@ for (i in 1:length(from)){
       # If city == city, then skip as distance will be 0.
       next
     }
-    # print (i)
-    # print (from[i])
-    # print (j)
-    # print (to[j])
-    k <- mapdist(from[i],to[j])[5]
+    #k <- mapdist(from[i],to[j])[5]
     ### Important, mapdist returns a Tibble w/ 9 columns.
     ### The fifth value has miles.
-    # print (k)
-
-    distances[x,1] <- from[i]
-    distances[x,2] <- to[j]
-    distances[x,3] <- k[5]
-    x <- x + 1
+    distances[x,1] <- from[i] # Add name of "from" city
+    distances[x,2] <- to[j] # Add name of "to" city
+    distances[x,3] <- mapdist(from[i],to[j])[5] # Add calcuated distance
+    distances[x,4] <- cities_top10$Population[i] # Add "from" population
+    distances[x,5] <- cities_top10$freq[i]
+    distances[x,6] <- cities_top10$num_cars[i]
+    distances[x,7] <- cities_top10$Population[j] # Add "to" population
+    distances[x,8] <- cities_top10$freq[j]
+    distances[x,9] <- cities_top10$num_cars[j]
+    x <- x + 1 # go to next row in Tibble
   }
-  
 }
 
-
+### Add 
